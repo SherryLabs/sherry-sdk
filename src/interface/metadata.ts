9afb20b821439102af0408b98d914181bc244c0c
Gilberts Ahumada <gilbertsahumada@gmail.com>
2024-11-24 18:21:23 -0300

    tests done for new interface

--- src/interface/metadata.ts
+++ src/interface/metadata.ts
@@ -49,9 +49,42 @@ export interface Metadata {
 }
 
 /**
- * Interface representing the final metadata for a mini app.
+ * Interface representing the validated metadata for a mini app.
  * 
- * This interface ensures that the actions are always of type `BlockchainAction[]`.
+ * This interface extends the `Metadata` interface but ensures that the `actions`
+ * property is always an array of `BlockchainAction` objects. This is used to
+ * represent the final state of the metadata after it has been processed and
+ * validated.
+ * 
+ * @extends {Omit<Metadata, 'actions'>}
+ * 
+ * @property {BlockchainAction[]} actions - An array of `BlockchainAction` objects,
+ * each defining a specific action that can be performed by the mini app.
+ * 
+ * @example
+ * ```typescript
+ * const validatedMetadata: ValidatedMetadata = {
+ *   type: "action",
+ *   icon: "icon.png",
+ *   title: "My Mini App",
+ *   description: "This is a mini app example",
+ *   actions: [
+ *     {
+ *       label: "Test Action",
+ *       contractAddress: "0x1234567890abcdef1234567890abcdef12345678",
+ *       contractABI: exampleAbi,
+ *       functionName: "safeTransferFrom",
+ *       chainId: "ethereum",
+ *       transactionParameters: [
+ *         { name: "from", type: "address" },
+ *         { name: "to", type: "address" },
+ *         { name: "tokenId", type: "uint256" }
+ *       ],
+ *       blockchainActionType: "nonpayable"
+ *     }
+ *   ]
+ * };
+ * ```
  */
 export interface ValidatedMetadata extends Omit<Metadata, 'actions'> {
   actions: BlockchainAction[];
