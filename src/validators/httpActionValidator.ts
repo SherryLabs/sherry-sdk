56612ca981fdadf9982859f4b3f7c7ff3da72dc6
Gilberts Ahumada <gilbertsahumada@gmail.com>
2025-03-17 10:34:51 -0300

    ci/cd: setting up config

--- src/validators/httpActionValidator.ts
+++ src/validators/httpActionValidator.ts
@@ -155,7 +155,7 @@ export class HttpActionValidator {
     }
 
     private static isValidEmail(value: string): boolean {
-        const emailRegex = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/;
+        const emailRegex = /^[\w-]+@([\w-]+\.)+[\w-]{2,4}$/;
         return emailRegex.test(value);
     }
 
