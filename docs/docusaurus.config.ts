2fcd539ac78c09eb7165fef32f256bbbc41ae7bf
Gilberts Ahumada <gilbertsahumada@gmail.com>
2025-05-22 12:44:06 +0100

    upating output dir

--- docs/docusaurus.config.ts
+++ docs/docusaurus.config.ts
@@ -14,7 +14,7 @@ const config: Config = {
     url: 'https://sherry.social',
     // Set the /<baseUrl>/ pathname under which your site is served
     // For GitHub pages deployment, it is often '/<projectName>/'
-    baseUrl: '/sherry-sdk/', // Changed baseUrl for GitHub Pages
+    baseUrl: '/', // Changed baseUrl for GitHub Pages
 
     // GitHub pages deployment config.
     // If you aren't using GitHub pages, you don't need these.
