83a5378c0b77e6d05d090ffadf34b03f530942b1
Gilberts Ahumada <gilbertsahumada@gmail.com>
2025-06-15 18:24:26 +0100

    removing icons from docs

--- docs/sidebars.ts
+++ docs/sidebars.ts
@@ -83,6 +83,7 @@ const sidebars: SidebarsConfig = {
             ],
         },
         // 4. Integrate Us Section
+        /*
         {
             type: 'category',
             label: 'Integrate Sherry',
@@ -96,11 +97,9 @@ const sidebars: SidebarsConfig = {
             items: [
                 'integrate-us/trigger-ui',
                 'integrate-us/trigger-ui-extension', // Overview of the SDK
-                //'integrate-us/sdk-setup', // Setting up the SDK
-                //'integrate-us/sdk-integration', // Integrating the SDK into your project
-                //'integrate-us/sdk-configuration', // Configuring the SDK
             ],
         },
+        */
         // 5. Guides Section
         {
             type: 'category',
